e70c75d43bf4507ab0d5e3ab9020572db84e321d
Mohammad Saadat <mohammad2682593@gmail.com>
2020-11-23 12:09:18 +0330

    initial commit

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: افزونه پرداخت امن ایران درگاه برای ووکامرس
+Version: 0.9
+Description:  افزونه درگاه پرداخت امن ایران درگاه برای فروشگاه ساز ووکامرس
+Plugin URI: https://irandargah.com
+
+*/
+include_once("class-wc-gateway-irandargah.php");
