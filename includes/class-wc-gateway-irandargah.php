c2061bfcbe96d2c05a5ee78504c4881be4b0c0f3
S-Haghighat <74814698+S-Haghighat@users.noreply.github.com>
2021-09-18 21:30:46 +0430

    Update class-wc-gateway-irandargah.php
    fixed "iteration" and added some changes in "send_curl_request" function

--- includes/class-wc-gateway-irandargah.php
+++ includes/class-wc-gateway-irandargah.php
@@ -471,14 +471,17 @@ class WC_Gateway_IranDargah extends WC_Payment_Gateway
     private function send_curl_request($url, $data)
     {
         $response = null;
-        $iteration = 3;
+        $iteration = 0;
 
         do {
 
             $curl = curl_init();
             curl_setopt_array($curl, array(
                 CURLOPT_URL => $url,
+                CURLOPT_POST => 1,
                 CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_SSL_VERIFYHOST => 0,
+                CURLOPT_SSL_VERIFYPEER => 0,
                 CURLOPT_FOLLOWLOCATION => false,
                 CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
                 CURLOPT_TIMEOUT => 15,
